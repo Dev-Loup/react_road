d74b6b1a7adff420f0014d498600fe958253419d
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2021-01-13 22:28:42 -0500

    [add]: react road filtering lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+const List = ({ list }) =>
+  list.map((item) =>
+      <Item
+      key={item.objectID}
+      {...item}
+    />
+  )
+
+const Item = ({ title, url, author, num_comments, points }) =>
+  <div>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </div>
+
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(localStorage.getItem('value') || initialState);
+  React.useEffect(() => localStorage.setItem('value', value), [value, key]);
+  return [ value, setValue ]
+}
+
+const InputWithLabel = ({id, value, onInputChange, children, type='text', }) =>
+  <>
+    <label htmlFor={id}>{children}</label>
+    &nbsp;
+    <input
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
+    />
+  </>
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React')
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  const handleSearch = event => {
+    setSearchTerm(event.target.value)
+  }
+  const filterStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase()))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>Hello</h1>
+      <InputWithLabel
+        id='search'
+        value={searchTerm}
+        onInputChange={handleSearch}
         >
-          Learn React
-        </a>
-      </header>
+        <strong>Search:</strong>
+        </InputWithLabel>
+      <hr />
+      < List list={filterStories} />
     </div>
   );
 }
-
 export default App;
